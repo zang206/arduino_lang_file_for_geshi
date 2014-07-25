0b2996442b13688ab64df85edc47a84090faa744
zang206 <jason@procknation.com>
2014-07-24 20:04:53 -0500

    changes to the preamble comments and readme file
    Moved some of the comments from arduino.php to the README.md file.

--- README.md
+++ README.md
@@ -1,4 +1,51 @@
 arduino_lang_file_for_geshi
 ===========================
 
-Arduino language file for geshi syntax highlighting
+This is an Arduino language file for the GeSHi syntax highlighter.
+
+The original file was downloaded from 
+http://stuff.nekhbet.ro/2009/08/18/free-arduino-language-file-for-geshi.html.
+
+Below is the original preamble of the file including all the attributions 
+to the authors and contributors plus licensing and copyright information.
+
+/*************************************************************************************
+ * arduino.php over c.php (used as a template)
+ * -----
+ * Author: Trimbitas Sorin-Iulian (trimbitassorin@hotmail.com)
+ * Contributors:
+ *	- Nigel McNie (nigel@geshi.org) - the original c.php
+ * Copyright: (c) 2009 Trimbitas Sorin-Iulian (http://tutorialpedia.org/)
+ * Release Version: 1.0.0
+ * Date Started: 2009/08/09
+ *
+ * Arduino language file for GeSHi.
+ *
+ * CHANGES
+ * -------
+ *  2009/08/09 (1.0.0)
+ *   -  First Release
+ *
+ * TODO (updated 2009/02/08)
+ * -------------------------
+ *  -  none for now
+ *
+ *************************************************************************************
+ *
+ *     This file is part of GeSHi.
+ *
+ *   GeSHi is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   GeSHi is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with GeSHi; if not, write to the Free Software
+ *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ ************************************************************************************/
--- arduino.php
+++ arduino.php
@@ -18,52 +18,14 @@
 * one location and have their additions and changes properly 
 * attributed to them. 
 *
-* The rest of this file is the same as when I downloaded it. All further
-* changes will now be tracked on the GitHub site.
+* Check the repo README.md file for information about the original authors and
+* contributors. Any further changes will now be tracked and documented on the 
+* GitHub site, not in this file.
 *
 * Thanks, 
 * Jason Procknow jprocknow@gmail.com http://procknation.com
-*/
-/*************************************************************************************
- * arduino.php over c.php (used as a template)
- * -----
- * Author: Trimbitas Sorin-Iulian (trimbitassorin@hotmail.com)
- * Contributors:
- *	- Nigel McNie (nigel@geshi.org) - the original c.php
- * Copyright: (c) 2009 Trimbitas Sorin-Iulian (http://tutorialpedia.org/)
- * Release Version: 1.0.0
- * Date Started: 2009/08/09
- *
- * Arduino language file for GeSHi.
- *
- * CHANGES
- * -------
- *  2009/08/09 (1.0.0)
- *   -  First Release
- *
- * TODO (updated 2009/02/08)
- * -------------------------
- *  -  none for now
- *
- *************************************************************************************
- *
- *     This file is part of GeSHi.
- *
- *   GeSHi is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
- *   GeSHi is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with GeSHi; if not, write to the Free Software
- *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- ************************************************************************************/
+*****************************************************************************************/
+
 
 $language_data = array (
     'LANG_NAME' => 'Arduino',
